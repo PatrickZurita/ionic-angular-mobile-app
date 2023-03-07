c06679374eb4ac600e22716937ba0453a41897af
Patrick Zurita <71539616+PatrickZurita@users.noreply.github.com>
2023-03-07 15:12:42 -0500

    hello world ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor() { }
+
+  ngOnInit() {
+    console.log('test');
+  }
 
 }
